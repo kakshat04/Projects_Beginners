afe084fc992eee0ee9cab2df5b43bfd734857c41
Kumar Akshat <kumar.akshat1988@gmail.com>
2020-04-05 01:14:08 +0530

    Dice Rolling Game

--- Dice_Rolling_Simulator.py
+++ Dice_Rolling_Simulator.py
@@ -6,14 +6,46 @@ It should then ask you if you’d like to roll again. For this project, you’ll
 that your dice can produce. For the average die, that means a minimum of 1 and a maximum of 6. You’ll also want a
 function that randomly grabs a number within that range and prints it.
 Concepts to keep in mind:
-
-Random
-Integer
-Print
-While Loops
-
-
-A good project for beginners, this project will help establish a solid foundation for basic concepts.
-And if you already have programming experience, chances are that the concepts used in this project aren’t
-completely foreign to you. Print, for example, is similar to Javascript’s console.log.
 """
+
+import random
+import time
+
+
+class DiceRolling:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Object of Rolling Dice Class"
+
+    def start_game(self):
+        print("Welcome to Dice Rolling Game..")
+        time.sleep(.5)
+        dice_values_lst = [1, 2, 3, 4, 5, 6]
+        dice_number = random.choice(dice_values_lst)
+        print(f"Number is :: {dice_number}")
+        time.sleep(.5)
+
+        user_input = "YES"
+        max_chances = 0
+        while user_input == "YES":
+            user_input = input("Do you want to try another time ? -- Yes/No :: ").upper()
+            if user_input not in ["YES", "NO"]:
+                return "Please enter in only Yes or No"
+            elif max_chances == 4:
+                return "Maximum Trials allowed are 5. Please run the program again for more trials"
+            elif user_input == "YES":
+                dice_number = random.choice(dice_values_lst)
+                print(f"Number is :: {dice_number}")
+                time.sleep(.5)
+            max_chances += 1
+
+        return "Thank you for playing.."
+
+
+if __name__ == '__main__':
+    dice_obj = DiceRolling()
+    print(dice_obj)
+    print(dice_obj.start_game())
+
